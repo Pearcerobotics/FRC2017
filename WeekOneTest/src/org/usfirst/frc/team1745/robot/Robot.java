732fad085f9c01d54a76ec82cc3974bb50d74434
Sanjay <Sanjay@DESKTOP-TNMNBK6.lan>
2017-02-18 20:04:47 -0800

    Just got home, sorry for being late

--- WeekOneTest/src/org/usfirst/frc/team1745/robot/Robot.java
+++ WeekOneTest/src/org/usfirst/frc/team1745/robot/Robot.java
@@ -6,6 +6,7 @@ import org.usfirst.frc.team1745.robot.P51Talon.Breakers;
 import org.usfirst.frc.team1745.robot.P51Talon.Motors;
 
 import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -34,16 +35,22 @@ public class Robot extends IterativeRobot {
 	P51Talon LFDrive, RFDrive, LMDrive, RMDrive, LBDrive, RBDrive, GearIntake, LeftSpinner, RightSpinner, lifter;
 	RobotDrive driveTrain;
 	BallShifter rightDrive, leftDrive;
-	Joystick joy1, joy2;
+	Joystick joy1, joy2, joy3;
 	// Pneumatics pneumatics;
 	DoubleSolenoid shifter, grabber, arm;
 	PowerDistributionPanel pdp;
 	boolean highGear;
-	DigitalInput beam;
+	DigitalInput frontBeam, backBeam;
+	double grabbedTime;
+	boolean isInWaitingMode = false;
+	boolean intake = false, holding = true, placing = false;
+	AnalogInput ultrasonicSensor;
 
 	private static final int IMG_WIDTH = 640;
 	private static final int IMG_HEIGHT = 480;
+	private static final double SONIC_RATE = 5 / 512;
 	NetworkTable table;
+	double[] heights, centers;
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -73,15 +80,18 @@ public class Robot extends IterativeRobot {
 		leftDrive = new BallShifter(LFDrive, LMDrive, LBDrive);
 		driveTrain = new RobotDrive(LFDrive, RFDrive);
 		driveTrain.setInvertedMotor(MotorType.kRearLeft, true);
-        driveTrain.setInvertedMotor(MotorType.kRearRight, true);
+		driveTrain.setInvertedMotor(MotorType.kRearRight, true);
 		joy1 = new Joystick(0);
 		joy2 = new Joystick(1);
+		joy3 = new Joystick(2);
 		// pneumatics = new Pneumatics();
 		shifter = new DoubleSolenoid(0, 1);
 		grabber = new DoubleSolenoid(2, 3);
 		arm = new DoubleSolenoid(4, 5);
 		highGear = false;
-
+		frontBeam = new DigitalInput(0);
+		backBeam = new DigitalInput(1);
+		ultrasonicSensor = new AnalogInput(2);
 	}
 
 	/**
@@ -119,6 +129,11 @@ public class Robot extends IterativeRobot {
 	}
 
 	public void teleopInit() {
+
+		intake = false;
+		holding = true;
+		placing = false;
+		SmartDashboard.putString("Mode", "holding");
 	}
 
 	/**
@@ -126,9 +141,10 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		double[] heights, centers;
-		heights = table.getNumberArray("height", new double[] { -1 });
-		centers = table.getNumberArray("centerX", new double[] { -1 });
+		SmartDashboard.putNumber("Ultrasound Distance", ultrasonicSensor.getVoltage() / SONIC_RATE);
+		manageStates();
+		actOnState();
+		pullTables();
 		double centerX = 320;
 		double distance = 0;
 		if (centers.length >= 2 && heights.length >= 2) {
@@ -144,12 +160,117 @@ public class Robot extends IterativeRobot {
 
 		double turn = (centerX - IMG_WIDTH / 2);
 
-		// Code from Nathan to determine distance and angle to the target
-		// constants
-		final double HORIZONTAL_FIELD_OF_VIEW = 50.6496; // degrees (also found
-															// 59.7, we should
-															// calibrate)
-		final double VERTICAL_FIELD_OF_VIEW = 39.3072; // degrees TODO calibrate
+		System.out.println("Turn: " + turn + " CenterX: " + centerX);
+		if (joy1.getTrigger()) {
+			driveTrain.arcadeDrive(-.75, turn * .005);
+		} else {
+			driveTrain.tankDrive(joy1.getY(), joy2.getY());
+		}
+
+		// Gear Control
+		if (joy2.getTrigger()) {
+			shifter.set(DoubleSolenoid.Value.kForward);
+		} else {
+			shifter.set(DoubleSolenoid.Value.kReverse);
+		}
+
+		if (joy1.getRawButton(3)) {
+			lifter.set(1);
+		} else {
+			lifter.set(0);
+		}
+
+		talonsToDashboard();
+
+	}
+
+	private void manageStates() {
+		// TODO Auto-generated method stub
+		if (joy3.getRawButton(1)) {
+			intake = true;
+			holding = false;
+			placing = false;
+			SmartDashboard.putString("Mode", "intake");
+		}
+		if (joy3.getRawButton(2)) {
+			intake = false;
+			holding = true;
+			placing = false;
+			SmartDashboard.putString("Mode", "holding");
+		}
+		if (joy3.getRawButton(3)) {
+			intake = false;
+			holding = false;
+			placing = true;
+			SmartDashboard.putString("Mode", "placing");
+		}
+	}
+	
+	public void actOnState(){
+		if (intake == true) {
+			System.out.println("I should be in intake");
+			GearIntake.set(1);
+			//TODO arm and grabber are one another...change names in code
+			arm.set(DoubleSolenoid.Value.kForward);
+			grabber.set(DoubleSolenoid.Value.kReverse);
+			LeftSpinner.set(-0.5);
+			RightSpinner.set(0.5);
+			
+			if (!frontBeam.get()) {
+
+				System.out.println("I should be in intake - break beam broken");
+				if (!isInWaitingMode) {
+					grabbedTime = System.currentTimeMillis();
+					isInWaitingMode = true;
+				}
+				arm.set(DoubleSolenoid.Value.kReverse);
+				if (System.currentTimeMillis() >= grabbedTime + 1000) {
+					GearIntake.set(0);
+				}
+			}
+		}
+		if (holding == true) {
+
+			System.out.println("I should be in holding");
+			GearIntake.set(0);
+			LeftSpinner.set(0);
+			RightSpinner.set(0);
+			grabber.set(DoubleSolenoid.Value.kReverse);
+			arm.set(DoubleSolenoid.Value.kForward);
+		}
+		if (placing == true) {
+		System.out.println("I should be in place");
+			GearIntake.set(0);
+			LeftSpinner.set(0);
+			RightSpinner.set(0);
+			grabber.set(DoubleSolenoid.Value.kForward);
+			arm.set(DoubleSolenoid.Value.kReverse);
+		}
+	}
+
+	public void pullTables(){
+		heights = table.getNumberArray("height", new double[] { -1 });
+		centers = table.getNumberArray("centerX", new double[] { -1 });
+	}
+	/**
+	 * This function is called periodically during test mode
+	 */
+	@Override
+	public void testPeriodic() {
+
+	}
+
+	public void talonsToDashboard() {
+		LFDrive.toDashboard();
+		RFDrive.toDashboard();
+	}
+
+	public void nathanAuto() {
+		pullTables();
+		final double HORIZONTAL_FIELD_OF_VIEW = 59.7; // degrees (also found
+		// 59.7, we should
+		// calibrate)
+		final double VERTICAL_FIELD_OF_VIEW = 41.4; // degrees TODO calibrate
 		final double DEGREES_TO_RADIANS = 0.01745329252;
 		final double RADIANS_TO_DEGREES = 57.295779513;
 		final double TARGET_HEIGHT_INCHES = 5.0; // inches
@@ -160,7 +281,7 @@ public class Robot extends IterativeRobot {
 		// calculations
 		double averageCenter = 0.0;
 		double averageHeight = 0.0;
-		
+
 		boolean isDistanceValid = false;
 
 		// Get average center and height
@@ -179,16 +300,14 @@ public class Robot extends IterativeRobot {
 		double absoluteAngle = averageCenter / IMG_WIDTH * HORIZONTAL_FIELD_OF_VIEW;
 		double relativeAngle = absoluteAngle - HORIZONTAL_FIELD_OF_VIEW / 2.0;
 
-		if(isDistanceValid)
-		{
+		if (isDistanceValid) {
 			System.out.println("Relative Angle from robot to target (degrees): " + relativeAngle);
 		}
 
 		double distanceCalculationFRC = TARGET_HEIGHT_INCHES * IMG_HEIGHT
 				/ (2 * averageHeight * Math.tan((VERTICAL_FIELD_OF_VIEW / 2) * DEGREES_TO_RADIANS));
 
-		if(isDistanceValid)
-		{
+		if (isDistanceValid) {
 			System.out.println("Distance from robot to target (inches): " + distanceCalculationFRC);
 		}
 
@@ -196,9 +315,9 @@ public class Robot extends IterativeRobot {
 		double[] widths;
 		widths = table.getNumberArray("width", new double[] { -1 });
 		double averageWtH = -1.0;
-		
+
 		boolean isWidthToHeightValid = false;
-		
+
 		if (widths.length >= 2 && heights.length >= 2) {
 			averageWtH = ((widths[0] / heights[0]) + (widths[1] / heights[1])) / 2.0;
 			isWidthToHeightValid = true;
@@ -208,8 +327,7 @@ public class Robot extends IterativeRobot {
 		} else {
 			// do nothing
 		}
-		if(isWidthToHeightValid)
-		{
+		if (isWidthToHeightValid) {
 			System.out.println("Average width to height ratio: " + averageWtH);
 		}
 
@@ -222,44 +340,10 @@ public class Robot extends IterativeRobot {
 			amountToTurn = 0.0;
 		}
 
-		if(isWidthToHeightValid)
-		{
+		if (isWidthToHeightValid) {
 			System.out.println("!!NathanDrive!! turn: " + amountToTurn);
 		}
 
-		System.out.println("Turn: " + turn + " CenterX: " + centerX);
-		if (joy1.getTrigger()) {
-			driveTrain.arcadeDrive(-.75, -turn * .005);
-		} else {
-			driveTrain.tankDrive(joy1.getY(), joy2.getY());
-		}
-		
-		//Lifter control
-		if(joy2.getTrigger()){
-			shifter.set(DoubleSolenoid.Value.kForward);
-		} else {
-			shifter.set(DoubleSolenoid.Value.kReverse);
-		}
-		
-		if (joy1.getRawButton(3)){
-			lifter.set(1);
-		} else {
-			lifter.set(0);
-		}
-
-	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-
-	}
-
-	public void talonsToDashboard() {
-		LFDrive.toDashboard();
-		RFDrive.toDashboard();
 	}
 
 }
